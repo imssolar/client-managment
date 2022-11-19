35d3f8e062e9848ac6fb7e350ed2bea543c87c7c
Imssolar <i.montecinosssolar@gmail.com>
2022-11-19 19:30:40 -0300

    Update new Appbar

--- src/components/UpperMenu/UpperMenu.tsx
+++ src/components/UpperMenu/UpperMenu.tsx
@@ -1,18 +1,35 @@
-import { Button, Container, Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import { BiExit } from 'react-icons/bi'
 
 export const UpperMenu = () => {
+	const handleClick = () => {
+		console.log('Salir!')
+	}
+
 	return (
-		<Container maxWidth="lg">
-			<Grid container>
-				<Grid item>
-					<Typography>COMPAÑIA PRUEBA</Typography>
-				</Grid>
-				<Grid>
-					<Typography>NOMBRE USUARIO</Typography>
-					<Button variant="contained">SALIR</Button>
-				</Grid>
-			</Grid>
-		</Container>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar
+				position="static"
+				sx={{ borderBottom: '3px solid dodgerblue', backgroundColor: 'navy' }}
+			>
+				<Toolbar>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						MANTENEDOR DE CLIENTES -
+					</Typography>
+
+					<IconButton
+						size="medium"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2, backgroundColor: 'whitesmoke' }}
+						onClick={handleClick}
+					>
+						<BiExit style={{ color: 'black' }} />
+					</IconButton>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	)
 }
